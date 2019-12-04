f3f7ec1fb1ae830f3a0a69279ea3979a98fb882a
PaulinaArvizu <is707571@iteso.mx>
2019-12-04 15:03:10 -0600

    pedido Schema

---
+++ db/pedido.js
@@ -0,0 +1,31 @@
+const mongoose = require('./mongodb-connect') //importa el archivo
+
+let pedidoSchema = mongoose.Schema({
+    productos: {
+        type: String,
+        required: true
+    },
+    subtotal: {
+        type: Number,
+        required: true
+    },
+    total: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: [1, 2, 3],
+        required: true
+    }
+});
+
+pedidoSchema.statics.addPedido = function(pedido) {
+    console.log(pedido);
+    let newPedido = Pedido(pedido);
+    return newPedido.save();
+}
+
+let Pedido = mongoose.model('pedido', pedidoSchema);
+
+module.exports = Pedido;
