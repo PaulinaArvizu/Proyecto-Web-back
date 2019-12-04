90ad97b6c5e5263f4cb9593eba3004bc3b38b6f2
PaulinaArvizu <is707571@iteso.mx>
2019-12-04 15:02:50 -0600

    metodoPago Schema

---
+++ db/metodoPago.js
@@ -0,0 +1,48 @@
+const mongoose = require('./mongodb-connect') //importa el archivo
+
+let metodoPagoSchema = mongoose.Schema({
+    tarjeta: {
+        type: String,
+        required: true
+    },
+    tipo: {
+        type: String,
+        enum: ['Debito', 'Credito'],
+        required: true
+    },
+    categoria: {
+        type: String,
+        enum: ['Mastercard', 'Visa', 'American Express'],
+        required: true
+    },
+    fecha: {
+        type: Date,
+        required: true
+    },
+    nombre: {
+        type: String,
+        required: true
+    },
+    apellido: {
+        type: String,
+        required: true
+    },
+    codigo: {
+        type: String,
+        required: true
+    },
+    direccion: {
+        type: [String], //arreglo de strings
+        required: true
+    }
+});
+
+metodoPagoSchema.statics.addMetodoPago = function(metodoPago) {
+    console.log(metodoPago);
+    let newMetodoPago = User(metodoPago);
+    return newMetodoPago.save();
+}
+
+let MetodoPago = mongoose.model('metodoPago', metodoPagoSchema);
+
+module.exports = MetodoPago;
